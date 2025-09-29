8f492f904564035a7b57f7199cc46fc9d477284d
Adithya761 <susmithasamvardhinil@gmail.com>
2025-09-29 09:17:54 +0530

    fiile.c added succesfully

---
+++ file.c
@@ -0,0 +1,6 @@
+#include <>[Dstdio.h[>
+int main(){}[D
+[A[C[Cvoid [C[C[D[D main[C
+[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C
+[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[Dprintf()[D""[B[D[A[D[CHELLO WORLD!");
+}
